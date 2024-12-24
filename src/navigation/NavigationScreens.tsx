9dad803f5538e029cf9065954c4aeeb7ba017dbc
Rafael Dallaqyan <rafaeldallaqyan219@gmail.com>
2024-12-24 17:05:57 +0400

    couple of changes in navigation

--- src/navigation/NavigationScreens.tsx
+++ src/navigation/NavigationScreens.tsx
@@ -1,42 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {TabScreens} from './';
-import {NewTaskScreen} from '../pages';
 
-const Stack = createStackNavigator<any>();
+const Stack = createNativeStackNavigator();
 
 export function NavigationScreens() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {/* <Stack.Group> */}
-          <Stack.Screen
-            name="NewTaskScreen"
-            component={TabScreens}
-            options={{
-              headerShown: false,
-              cardStyle: {backgroundColor: '#FFFFFF'},
-            }}
-          />
-          {/* </Stack.Group> */}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="NewTaskScreen"
+          component={TabScreens}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const style = StyleSheet.create({
-  titleStyle: {
-    color: '#333333',
-    fontFamily: 'NotoSansArmneian-SemiBold',
-    fontWeight: '600',
-    fontSize: 24,
-    lineHeight: 32,
-    paddingTop: 15,
-    textAlign: 'center',
-  },
-});
